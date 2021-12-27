6e815408ab52286043f6b5e1253052332e295bbb
byusa <byusa.serge@gmail.com>
2021-12-27 11:22:09 -0800

    before the crash

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'graphene_django',
     'tracks'
 ]
 
+GRAPHENE = {
+    'SCHEMA': 'app.schema.schema'
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
