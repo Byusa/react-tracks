c3a9f6df63128890128e7f1ca38809884a243cbb
byusa <byusa.serge@gmail.com>
2021-12-29 10:29:33 -0800

    before adding graphene_django on the installed_apps

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
